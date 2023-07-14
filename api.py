6ae0c0b73087e930d95c04aefe1314dcc0f4073a
vu9ar <vabdulaliyev@gmail.com>
2023-07-14 07:12:03 +0400

    commit

--- api.py
+++ api.py
@@ -26,49 +26,57 @@ class VARIABLES:
 
 
 class ModelInput(BaseModel):
-    Annual_Income: Optional[float64] = None
-    Monthly_Inhand_Salary: Optional[float64] = None
-    Num_Bank_Accounts: Optional[float64] = None
-    Num_Credit_Card: Optional[float64] = None
-    Interest_Rate: Optional[float64] = None
-    Num_of_Loan: Optional[float64] = None
-    Delay_from_due_date: Optional[float64] = None
-    Num_of_Delayed_Payment: Optional[float64] = None
-    Changed_Credit_Limit: Optional[float64] = None
-    Num_Credit_Inquiries: Optional[float64] = None
+    Annual_Income: Optional[float] = None
+    Monthly_Inhand_Salary: Optional[float] = None
+    Num_Bank_Accounts: Optional[float] = None
+    Num_Credit_Card: Optional[float] = None
+    Interest_Rate: Optional[float] = None
+    Num_of_Loan: Optional[float] = None
+    Delay_from_due_date: Optional[float] = None
+    Num_of_Delayed_Payment: Optional[float] = None
+    Changed_Credit_Limit: Optional[float] = None
+    Num_Credit_Inquiries: Optional[float] = None
     Credit_Mix: Optional[object] = None
-    Outstanding_Debt: Optional[float64] = None
-    Credit_Utilization_Ratio: Optional[float64] = None
-    Credit_History_Age: Optional[float64] = None
-    Payment_of_Min_Amount:Optional[object]=None
-    Total_EMI_per_month: Optional[float64] = None
-    Amount_invested_monthly: Optional[float64] = None
-    Monthly_Balance: Optional[float64] = None
+    Outstanding_Debt: Optional[float] = None
+    Credit_Utilization_Ratio: Optional[float] = None
+    Credit_History_Age: Optional[float] = None
+    Payment_of_Min_Amount: Optional[object] = None
+    Total_EMI_per_month: Optional[float] = None
+    Amount_invested_monthly: Optional[float] = None
+    Monthly_Balance: Optional[float] = None
 
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
 
+    @classmethod
+    def validate(cls, v):
+        return v
 
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="number")
 
 model = pickle.load(open(VARIABLES.model_filename, "rb"))
 
 
 def preprocess(data: pd.DataFrame) -> pd.DataFrame:
     original_data = pd.read_excel(VARIABLES.model_input)
-    original_data = original_data.drop(columns = ['ID', 'Type_of_Loan', 'Payment_Behaviour', 'Age','SSN','Customer_ID', 'Occupation','Name', 'Month', 'Credit_Score'], axis = 1)
-    data = pd.concat([original_data, data], axis = 0)
-    data = data.reset_index(drop = True)
-    
-    data = pd.get_dummies(data, columns=['Payment_of_Min_Amount'], drop_first = True)
+    original_data = original_data.drop(columns=['ID', 'Type_of_Loan', 'Payment_Behaviour', 'Age', 'SSN', 'Customer_ID',
+                                                'Occupation', 'Name', 'Month', 'Credit_Score'], axis=1)
+    data = pd.concat([original_data, data], axis=0)
+    data = data.reset_index(drop=True)
+
+    data = pd.get_dummies(data, columns=['Payment_of_Min_Amount'], drop_first=True)
     le = LabelEncoder()
     data['Credit_Mix'] = le.fit_transform(data.Credit_Mix)
-
 
     col = data.columns
     scaler = MinMaxScaler()
     data = scaler.fit_transform(data)
-    data = pd.DataFrame(data, columns = col)
-    print(data)
+    data = pd.DataFrame(data, columns=col)
 
-    return data.iloc[-1,:].values.reshape(1,-1)
+    return data.iloc[-1, :].values.reshape(1, -1)
 
 
 @app.get("/predict")
